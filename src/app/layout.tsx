92158f00d343401eb8c57191963161f0657d4e50
Nikolaos Kladis <nikcladis@gmail.com>
2025-03-31 17:44:53 +0300

    🔧 Replace Geist font with Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./providers";
 import AuthErrorHandler from "@/shared/auth/error";
 import BaseLayout from "@/shared/layout/base-layout";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 flex flex-col min-h-screen`}
+        className={`${inter.variable} antialiased bg-gray-50 flex flex-col min-h-screen`}
       >
         <AuthProvider>
           <AuthErrorHandler />
